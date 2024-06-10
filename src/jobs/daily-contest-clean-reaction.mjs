c760a59762d4c6bc3b09ba8668ea712e93bfb091
Kazerlelutin <b@bouteiller.contact>
2024-06-10 18:37:46 +0200

    log

--- src/jobs/daily-contest-clean-reaction.mjs
+++ src/jobs/daily-contest-clean-reaction.mjs
@@ -7,7 +7,10 @@ let start = false
 
 export async function dailyContestCleanReaction() {
   // if (start) return
-  console.log('🚀 Lancement de la tâche DAILY CONTEST CLEAN REACTION')
+  console.log(
+    '🚀 Lancement de la tâche DAILY CONTEST CLEAN REACTION',
+    new Date()
+  )
 
   const channel = client.channels.cache.get(process.env.CHANNEL_BLABLA_ID)
   if (!channel) return
