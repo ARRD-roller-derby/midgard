6903f5ee077098376744cd978200928eded0f54f
Kazerlelutin <b@bouteiller.contact>
2025-01-25 00:09:53 +0100

    change horaire

--- index.mjs
+++ index.mjs
@@ -30,7 +30,7 @@ async function start() {
   // ===== DAILY =================================================================================================
   new CronJob('0 10 * * *', dailyContest, null, true, 'Europe/Paris')
   new CronJob('45 19 * * *', dailyContestResult, null, true, 'Europe/Paris')
-  new CronJob('30 19 * * *', dailyBadges, null, true, 'Europe/Paris')
+  new CronJob('15 21 * * *', dailyBadges, null, true, 'Europe/Paris')
   // ===== WEEKLY ===============================================================================================
   new CronJob('0 11 * * 1', weekRules, null, true, 'Europe/Paris')
 
